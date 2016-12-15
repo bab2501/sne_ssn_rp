5aca9b26c3dcccc8a9c650abeadf16aeb96658e3
Alexander Blaauwgeers <ablaauwgeers@desktop-40.students.os3.nl>
2016-12-15 13:19:11 +0100

    Final script after running and updated latex upto 14.12

--- scripts/ggwloop.py
+++ scripts/ggwloop.py
@@ -1,22 +1,31 @@
 import socket
+alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print "Socket Created"
-port = 9220
-ip = "192.168.1.120"
 
-s.connect ((ip, port))
-print "Socket Connected to HOST on ip "+ ip
+for letter1 in alphabet:
+	for letter2 in alphabet:
+		for letter3 in alphabet:
+			for letter4 in alphabet:
+				command = letter1+letter2+letter3+letter4
+				#print command+"\r\n"+"\r\n"
 
-reply = ''
-while True:
-    message = "OPEN\r\n"
-    reply += s.recv(1024)
-    if not reply:
-        break
-    #break
-    if '220 HP GGW server (version 1.0) ready' in reply:
-        s.sendall(message)
-        break
-noot= s.recv(65535)
-print noot
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				#print "Socket Created"
+				port = 9220
+				ip = "192.168.1.120"
+
+				s.connect ((ip, port))
+				#print "Socket Connected to HOST on ip "+ ip
+
+				reply = ''
+				while True:
+				    message = command+"\r\n"
+				    reply += s.recv(1024)
+				    if not reply:
+					break
+				    #break
+				    if '220 HP GGW server (version 1.0) ready' in reply:
+					s.sendall(message)
+					break
+				noot= s.recv(65535)
+				print noot
---
+++ scripts/ggwloop2.py
@@ -0,0 +1,22 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Socket Created"
+port = 9220
+ip = "192.168.1.120"
+
+s.connect ((ip, port))
+print "Socket Connected to HOST on ip "+ ip
+
+reply = ''
+while True:
+    message = "OPEN\r\n"
+    reply += s.recv(1024)
+    if not reply:
+        break
+    #break
+    if '220 HP GGW server (version 1.0) ready' in reply:
+        s.sendall(message)
+        break
+noot= s.recv(65535)
+print noot
